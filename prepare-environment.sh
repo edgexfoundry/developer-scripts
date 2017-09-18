9dac8b8286ceb5ec3a3c49661dfbcb66d471f960
chencho <sergio.munoz@caviumnetworks.com>
2017-09-18 17:20:52 +0200

    Added two scripts to prepare the environment automatically:
    prepare-environment.sh - Downloads all the modules necessary or you can
    select which module download.
    
    update-packages.sh - Looks for updates in the installed packages
    
    Signed-off-by: chencho <sergio.munoz@caviumnetworks.com>

---
+++ prepare-environment.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Copyright 2017 Cavium Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: chencho
+
+set -e
+
+MODULES=(core-data-client core-domain core-exception core-test export-distro export-domain export-test support-domain support-logging-client developer-scripts export-client support-notifications-client core-data core-metadata-client )
+
+installAllModules(){
+SELECTED_MODULES=($@)
+for m in ${SELECTED_MODULES[@]} ;  do
+    if [ -d $m ] ; then
+        echo "Updating $m"
+        cd $m
+        git pull
+        echo $m
+        cd ..
+    else
+        echo "Cloning $m"
+        git clone https://github.com/edgexfoundry/$m
+    fi
+done
+
+}
+
+usage(){
+	echo "Error, you must press 'y' or 'n'"
+	exit 0
+}
+
+echo -n "Do you want to install all modules or select from the list? (y/n) -> "
+read ans
+
+if [[ "$ans" == "y" ]]; then
+	installAllModules "${MODULES[@]}"
+elif [[ "$ans" == "n" ]]; then
+	for m in ${MODULES[@]} ; do
+		echo -n "Do you want to install $m? (y/n) -> "
+		read insmodule 
+		if [[ "$insmodule" == "y" ]]; then
+			MOD+=($m)
+		fi
+	done
+
+	echo "${MOD[@]} modules selected"
+	installAllModules "${MOD[@]}" 
+else
+	usage
+
+fi
+
+
+
+echo "Done!"
+
