fe9e35dbb332fbc4323f9b5395ef36f93e957654
Jim White <jpwhite_mn@yahoo.com>
2019-07-26 10:43:51 -0500

    Fixes #130 and provides consistent network aliases for all containers; upgrades all the relevent containers to dot release 1.0.1; adds a readme file to explain missing 1.0.0 compose file
    Signed-off-by: Jim White <jpwhite_mn@yahoo.com>

--- clean_mongo.js
+++ clean_mongo.js
@@ -1,25 +1,4 @@
-//###############################################################################
-//# Copyright 2016-2017 Dell Inc.
-//#
-//# Licensed under the Apache License, Version 2.0 (the "License");
-//# you may not use this file except in compliance with the License.
-//# You may obtain a copy of the License at
-//#
-//# http://www.apache.org/licenses/LICENSE-2.0
-//#
-//# Unless required by applicable law or agreed to in writing, software
-//# distributed under the License is distributed on an "AS IS" BASIS,
-//# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//# See the License for the specific language governing permissions and
-//# limitations under the License.
-//#
-//###############################################################################
-//EdgeX Mongo DB reset script
-//version 1
-//author:  Jim White
-//Create user for security service in Mongo
-db=db.getSiblingDB('metadata')
-db.auth('meta','password');
+db=db.getSiblingDB('metadata');
 db.addressable.remove({});
 db.command.remove({});
 db.device.remove({});
@@ -30,21 +9,17 @@ db.provisionWatcher.remove({});
 db.schedule.remove({});
 db.scheduleEvent.remove({});
 db=db.getSiblingDB('coredata')
-db.auth('core','password');
 db.valueDescriptor.remove({});
 db.reading.remove({});
 db.event.remove({});
 db=db.getSiblingDB('logging')
-db.auth('logging','password');
 db.logEntry.remove({});
 db=db.getSiblingDB('scheduler')
-db.auth('scheduler','password');
 db.interval.remove({});
 db.intervalAction.remove({});
 db=db.getSiblingDB('notifications')
-db.auth('notifications','password');
 db.notification.remove({});
 db.subscription.remove({});
 db.transmission.remove({});
 db=db.getSiblingDB('exportclient')
-db.exportConfiguration.remove({});
+db.exportconfiguration.remove({});
