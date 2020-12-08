f5e9c561a3d1d5e22203c5d384e3c0849841f684
lenny <leonard.goodell@intel.com>
2020-12-08 16:16:55 -0700

    feat: Fixup. Changed to providing ID via uuidgen to avoid /r in retuned ID
    Signed-off-by: lenny <leonard.goodell@intel.com>

--- compose-builder/get-api-gateway-token.sh
+++ compose-builder/get-api-gateway-token.sh
@@ -12,13 +12,14 @@ fi
 
 rm -rf keys
 mkdir keys
-openssl genpkey -algorithm RSA -out keys/rsa4096.key -pkeyopt rsa_keygen_bits:4096
-openssl rsa -in keys/rsa4096.key -pubout -out keys/rsa4096.pub
-JWT=`docker run --rm -it -e KONGURL_SERVER=kong --network edgex_edgex-network --entrypoint "" -v ${PWD}/keys:/keys \
-       ${CORE_EDGEX_REPOSITORY}/docker-security-proxy-setup-go${ARCH}:${CORE_EDGEX_VERSION}${DEV} \
-        /edgex/secrets-config proxy adduser --token-type jwt --algorithm RS256 --public_key /keys/rsa4096.pub --user testinguser`
+openssl genpkey -algorithm RSA -out keys/rsa4096.key -pkeyopt rsa_keygen_bits:4096 2> /dev/null
+openssl rsa -in keys/rsa4096.key -pubout -out keys/rsa4096.pub 2> /dev/null
+ID=`uuidgen`
 docker run --rm -it -e KONGURL_SERVER=kong --network edgex_edgex-network --entrypoint "" -v ${PWD}/keys:/keys \
        ${CORE_EDGEX_REPOSITORY}/docker-security-proxy-setup-go${ARCH}:${CORE_EDGEX_VERSION}${DEV} \
-       /edgex/secrets-config proxy jwt --algorithm RS256 --id ${JWT} --private_key /keys/rsa4096.key
+        /edgex/secrets-config proxy adduser --token-type jwt --id ${ID} --algorithm RS256 --public_key /keys/rsa4096.pub --user testinguser > /dev/null
+docker run --rm -it -e KONGURL_SERVER=kong --network edgex_edgex-network --entrypoint "" -v ${PWD}/keys:/keys \
+       ${CORE_EDGEX_REPOSITORY}/docker-security-proxy-setup-go${ARCH}:${CORE_EDGEX_VERSION}${DEV} \
+       /edgex/secrets-config proxy jwt --algorithm RS256 --id ${ID} --private_key /keys/rsa4096.key
 rm -rf keys
 
