8d1c937bdfa8918513198da9486ed5b84de6f885
lenny <leonard.goodell@intel.com>
2020-12-08 15:30:57 -0700

    feat: fixup. Parameterized docker image url as was in the makefile
    Signed-off-by: lenny <leonard.goodell@intel.com>

--- compose-builder/get-api-gateway-token.sh
+++ compose-builder/get-api-gateway-token.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+# DEV and ARCH are set in environment prior to calling script
+# example: DEV=-dev ARCH=-arm64 ./get-api-gateway-token.sh
+
+# versions are loaded from .env file
+. ./.env
+
+if [ "$DEV" = "-dev" ]; then
+  CORE_EDGEX_VERSION=0.0.0
+fi
+
+
 mkdir keys
 openssl genpkey -algorithm RSA -out keys/rsa4096.key -pkeyopt rsa_keygen_bits:4096
 openssl rsa -in keys/rsa4096.key -pubout -out keys/rsa4096.pub
-JWT=`docker run --rm -it -e KONGURL_SERVER=kong --network edgex_edgex-network --entrypoint "" \
-       -v ${PWD}/keys:/keys nexus3.edgexfoundry.org:10004/docker-security-proxy-setup-go:master \
+JWT=`docker run --rm -it -e KONGURL_SERVER=kong --network edgex_edgex-network --entrypoint "" -v ${PWD}/keys:/keys \
+       ${CORE_EDGEX_REPOSITORY}/docker-security-proxy-setup-go${ARCH}:${CORE_EDGEX_VERSION}${DEV} \
         /edgex/secrets-config proxy adduser --token-type jwt --algorithm RS256 --public_key /keys/rsa4096.pub --user testinguser`
-docker run --rm -it -e KONGURL_SERVER=kong --network edgex_edgex-network --entrypoint "" \
-       -v ${PWD}/keys:/keys nexus3.edgexfoundry.org:10004/docker-security-proxy-setup-go:master \
+docker run --rm -it -e KONGURL_SERVER=kong --network edgex_edgex-network --entrypoint "" -v ${PWD}/keys:/keys \
+       ${CORE_EDGEX_REPOSITORY}/docker-security-proxy-setup-go${ARCH}:${CORE_EDGEX_VERSION}${DEV} \
        /edgex/secrets-config proxy jwt --algorithm RS256 --id ${JWT} --private_key /keys/rsa4096.key
 rm -rf keys
 
